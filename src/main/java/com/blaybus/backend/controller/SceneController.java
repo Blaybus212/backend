aae43cc7252694357178362a368e281fb6a4c1b6
joonamin <joonamin44@gmail.com>
2026-02-10 00:03:50 +0900

    ✨ feat(scenes-detail): scenes detail api 추가

--- src/main/java/com/blaybus/backend/controller/SceneController.java
+++ src/main/java/com/blaybus/backend/controller/SceneController.java
@@ -3,10 +3,12 @@ package com.blaybus.backend.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.blaybus.backend.domain.scene.SceneCategory;
+import com.blaybus.backend.dto.SceneDetailResponse;
 import com.blaybus.backend.dto.SceneListOrder;
 import com.blaybus.backend.dto.SceneListResponse;
 import com.blaybus.backend.dto.SceneRankResponse;
@@ -51,6 +53,13 @@ public class SceneController {
 		SceneListOrder order) { // 가나다순 → alphabetical,
 								// 인기순 →popularity
 		SceneListResponse response = sceneService.getScenes(category, page, limit, query, order);
+		return ResponseEntity.ok(response);
+	}
+
+	@GetMapping("/scenes/{sceneId}")
+	public ResponseEntity<SceneDetailResponse> getSceneDetail(@PathVariable(name = "sceneId", required = true)
+	Long sceneId) {
+		SceneDetailResponse response = sceneService.getSceneDetail(sceneId);
 		return ResponseEntity.ok(response);
 	}
 }
