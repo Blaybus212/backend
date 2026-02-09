fe5b373f0578b2ae6fc802e5ee92314d32ba53ac
Chewon Kim <stellaa223@gmail.com>
2026-02-09 22:15:42 +0900

    Feat/add mock data (#44)
    * refactor(security): update permitted endpoints and remove ChatController
    
    - Updated SecurityConfig to restrict access by removing the chat endpoint from the permitted list.
    - Deleted ChatController as it is no longer needed in the application.
    
    * refactor(quiz): rename quiz class and delete quiz choice
    
    * feat: add & fix mock data
    
    * feat: rebase 마무리
    
    * feat(data loader): add scene rank data
    
    * fix: accept review
    
    ---------
    
    Co-authored-by: DaehyeonKim <qwertypotter@gachon.ac.kr>

--- src/main/java/com/blaybus/backend/repository/QuizRepository.java
+++ src/main/java/com/blaybus/backend/repository/QuizRepository.java
@@ -2,9 +2,11 @@ package com.blaybus.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.blaybus.backend.domain.quiz.SceneRanksQuiz;
+import com.blaybus.backend.domain.quiz.Quiz;
 
 /**
- * QuizRepository provides CRUD operations for SceneRanksQuiz entities.
+ * QuizRepository provides CRUD operations for Quiz entities.
  */
-public interface QuizRepository extends JpaRepository<SceneRanksQuiz, Long> {}
+public interface QuizRepository extends JpaRepository<Quiz, Long> {
+    boolean existsByQuestion(String question);
+}
